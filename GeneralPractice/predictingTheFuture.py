609281bf844f7ea9c64b01b70d4ee42464b4385f
ScarletteBel <83143055+ScarletteBel@users.noreply.github.com>
2023-01-04 22:27:22 -0500

    nlpJournal submitted

---
+++ GeneralPractice/predictingTheFuture.py
@@ -0,0 +1,17 @@
+#Predicting your future....
+
+person = ["Artur", "Marc", "Mary", "Dua"]
+
+destiny = [
+    "You will find 10,000 dollars in your friend's pocket",
+    "You will get a proposal tommorrow",
+    "You will adopt a doggy <3",
+    "You will visit Autralia",
+    "You will feel sick soon"
+    "You will have a new house"
+    "You will have good luck"
+    "You will be bitten by Boni"
+    ]
+
+for name in person
+
