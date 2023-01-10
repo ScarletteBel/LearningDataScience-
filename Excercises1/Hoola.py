65db92161f25aff48f9dc930662aa53a92742f3a
ScarletteBel <83143055+ScarletteBel@users.noreply.github.com>
2023-01-10 11:05:33 -0500

    py exercises removed

--- Excercises1/Hoola.py
+++
@@ -1,7 +0,0 @@
-# Empezamos con el mas basico de basicos pero que no puede faltar
-
-print("Hola Mundo")
-
-nombre = input("Hola! Ingresa tu nombre: ")
-
-print("Hola" + " " + nombre)
--- Excercises2/LikeTurtles.py
+++
@@ -1,8 +0,0 @@
-#Decir que las torctugas son chiditas...
-
-animalFavorito = input("Ingresa tu animal favorito: ")
-
-if "tortuga" in animalFavorito.lower() or "turtle" in animalFavorito.lower():
-    print("Tambien me gustan las tortugas")
-else:
-    print("Ese animal esta muy padre pero no como las tortugas")
