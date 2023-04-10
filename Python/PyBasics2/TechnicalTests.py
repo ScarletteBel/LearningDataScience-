8c6796b03419d4da5d0e17da1262d5a7de459026
ScarletteBel <83143055+ScarletteBel@users.noreply.github.com>
2023-04-09 22:12:59 -0400

    tech small tests started

--- Python/PyBasics2/TechnicalTests.py
+++ Python/PyBasics2/TechnicalTests.py
@@ -1,11 +1,13 @@
 #Write a function that takes a string as input and returns the number of the vowels...
 
+
 def many_vowels(s):
+    string_list = list(s)
     existing_vowels = []
     vowels = [ "A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
-    for c in s:
+    for c in string_list:
         if c in vowels:
-            existing_vowels = existing_vowels + c
+            existing_vowels.extend(c)
     
     len_list = len(existing_vowels)
     return len_list
